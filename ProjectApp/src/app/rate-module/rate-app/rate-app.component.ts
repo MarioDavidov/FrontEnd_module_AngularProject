c0064a4afd82ec6f316d8a2e96bbf8a1e2860dbc
Mario Davido <75420164+MarioDavidov@users.noreply.github.com>
2021-09-07 19:45:45 +0300

    like-dislike-review & hide delete button

--- ProjectApp/src/app/rate-module/rate-app/rate-app.component.ts
+++ ProjectApp/src/app/rate-module/rate-app/rate-app.component.ts
@@ -3,16 +3,21 @@ import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { IRate } from 'src/app/shared/rating';
 import { TaskService } from 'src/app/task.service';
 import { NgForm } from '@angular/forms';
+import { firebase } from 'firebaseui-angular';
 @Component({
   selector: 'app-rate-app',
   templateUrl: './rate-app.component.html',
   styleUrls: ['./rate-app.component.css']
 })
 export class RateAppComponent implements OnInit {
-  rate!: IRate;
+  get useruid(): string{
+    return this.taskService.uid
+  }
+  rate!: any;
   ratings!: any;
   uid!:string;
   PushKey!: string;
+     
   
   constructor(private db: AngularFireDatabase, private taskService: TaskService) {
     this.uid = taskService.uid
@@ -21,15 +26,17 @@ export class RateAppComponent implements OnInit {
     
    }
   
-  ngOnInit(): void {
-    this.rate = new IRate();
+   ngOnInit(): void {
+     this.rate = new IRate();
+
     
   }
   
 
   create() {
     this.taskService.createRating(this.rate);
-    
+    console.log(this.uid)
+    console.log(this.rate.key)
   }
   Rate(form: NgForm):void {
     if(form.invalid){
@@ -48,7 +55,7 @@ export class RateAppComponent implements OnInit {
       (<HTMLInputElement>document.getElementById("review-title")).innerText="Write a review" 
     }, 900);
     
-      
+    
     this.rate.userPushKey = this.taskService.uid
     this.rate.nameOfCreator = this.taskService.username
     this.rate.key = ""    
@@ -59,7 +66,8 @@ export class RateAppComponent implements OnInit {
   }
 
 
-  deleteRev(event: any){       
+  deleteRev(event: any){
+    console.log(event)       
     let keyToDel  = event.target.previousElementSibling.innerText                                        
     let userPushKey =  this.taskService.uid
     let key = event.target.previousSibling.previousSibling.innerText
@@ -67,10 +75,19 @@ export class RateAppComponent implements OnInit {
      if (userPushKey == key){      
       keyToDel = (keyToDel).replace(/\s/g, '')
       this.taskService.deleteRate(keyToDel)
-     }else{     
-      event.target.disabled = true
-       
      }
 
   }
+  like(event:any){
+    let rateKey = event.target.parentElement.parentElement.nextElementSibling.nextElementSibling.innerText   
+    rateKey = (rateKey).replace(/\s/g, '')       
+    this.db.object(`/rate/${rateKey}`).update({ 'likes': firebase.database.ServerValue.increment(1) })
+   
+  }
+  dislike(event:any){
+    let rateKey = event.target.parentElement.parentElement.nextElementSibling.nextElementSibling.innerText
+    rateKey = (rateKey).replace(/\s/g, '')
+    this.db.object(`/rate/${rateKey}`).update({ 'dislikes': firebase.database.ServerValue.increment(1) })
+  }
+
 }
